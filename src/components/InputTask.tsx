20e630530d5e5472a7c5c4356598954f8c576d96
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-10-24 14:05:48 -0300

    finalizando estilos Header e InputTask

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,11 +1,17 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './InputTask.module.css'
 
 export function InputTask() {
     return (
-        <input
-            className={styles.inputTask}
-            type="text"
-            placeholder='Adicione uma nova tarefa'
-        />
+        <div className={styles.container}>
+            <input
+                className={styles.inputTask}
+                type="text"
+                placeholder='Adicione uma nova tarefa'
+            />
+            <button className={styles.buttonAddTask}>
+                Criar <PlusCircle />
+            </button>
+        </div>
     )
 }
