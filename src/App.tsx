f5d70cf52d9c8dbe7e4dad303387b3b76f96e731
WagaoFC <wagnerferreira_07@hotmail.com>
2022-10-24 23:25:41 -0300

    finalizando todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,73 @@
-import { useState } from 'react'
-import { Header } from './components/Header'
-import { InputTask } from './components/InputTask'
-import { Tasks } from './components/Tasks'
-import styles from './App.module.css'
-import './global.css'
+import { useEffect, useState } from "react";
+import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
+
+const LOCAL_STORAGE_KEY = "todo:savedTasks";
 
 export interface ITask {
-    id: string;
-    title: string;
-    isCompleted: boolean;
+  id: string;
+  title: string;
+  isCompleted: boolean;
 }
 
-export function App() {
-    const [task, setTasks] = useState<ITask[]>([
-        {
-            id: 'teste',
-            title: 'teste',
-            isCompleted: true,
-        },
-        {
-            id: 'teste',
-            title: 'testefalse',
-            isCompleted: false,
-        }
-    ])
+function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
 
-    function addTask(taskTitle: string) {
-        setTasks([
-            ...task,
-            {
-                id: crypto.randomUUID(),
-                title: taskTitle,
-                isCompleted: false
-            }
-        ])
+  function loadSavedTasks() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (saved) {
+      setTasks(JSON.parse(saved));
     }
+  }
 
-    return (
-        <div className={styles.wrapper}>
-            <Header />
-            <InputTask onAddTask={addTask} />
-            <Tasks tasks={task} />
-        </div>
-    )
+  useEffect(() => {
+    loadSavedTasks();
+  }, []);
+
+  function setTasksAndSave(newTasks: ITask[]) {
+    setTasks(newTasks);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks));
+  }
+
+  function addTask(taskTitle: string) {
+    setTasksAndSave([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      },
+    ]);
+  }
+
+  function deleteTaskById(taskId: string) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasksAndSave(newTasks);
+  }
+
+  function toggleTaskCompletedById(taskId: string) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      return task;
+    });
+    setTasksAndSave(newTasks);
+  }
+
+  return (
+    <>
+      <Header onAddTask={addTask} />
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onComplete={toggleTaskCompletedById}
+      />
+    </>
+  );
 }
+
+export default App;
